eabc9176764d59f5a1afb838007e7c7fe9e15922
smk4411 <saimanojkumar63@gmail.com>
2024-06-18 18:51:02 +0530

    prac6 succ

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main"Hello, World!"(String[] args) throws Exception {
+        System.out.println("Helo, Wor!");
     }
 }
